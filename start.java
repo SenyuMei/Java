5a161f26cf1c0b4860809014ef752f74cf489aab
SenyuMei <SenYuMei@hotmail.com>
2025-02-14 10:00:00 +0100

    Create start.java

---
+++ start.java
@@ -0,0 +1,27 @@
+// para iniciar un programa en java se debe de empezar por
+
+public class CMyApplicaition {
+    public static void main (String[] args) {
+      
+    // codigo
+
+    }
+}
+
+// para crear nuevos strings
+
+String nombre_string1;
+String nombre_string2;
+
+// los strings cuentan como una variable como (int, float, etc..)
+
+nombre_string1 = new String ("Texto del string, ¡Hola!");
+nombre_string2 = new String ("Texto del string, ¡Adios!");
+
+// para combinar strings
+
+String nombre_combinacion = "Texto combinado: \n" +nombre_string1 + nombre_string2;
+
+// para imprimir 
+
+System.out.println("Texto a imprimir");
