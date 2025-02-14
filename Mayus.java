ac8c99861ebb1b93cbb2bf3f3e86951753e77da8
SenyuMei <SenYuMei@hotmail.com>
2025-02-14 10:09:52 +0100

    Create Mayus.java

---
+++ Mayus.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String palabra = "hola";
+      //declaro el string 
+        palabra = palabra.toUpperCase();
+      //paso el texto de mi string a mayusculas
+        System.out.println(palabra);
+      //imprimo la palabra
+    }
+}
