174029dca4007635804fed98f111d908c5ee69cf
SenyuMei <SenYuMei@hotmail.com>
2025-02-17 12:58:26 +0100

    Create print_prinln.java

---
+++ print_prinln.java
@@ -0,0 +1,18 @@
+// la diferencia entre 
+System.out.println();
+
+// y
+System.out.print();
+
+// es que ln salta una linea y print no
+// ej. 
+
+System.out.println("Hola");
+system.out.println(" mundo");
+// output:
+// Hola
+//  mundo
+
+System.out.print("Hola");
+System.out.print(" mundo");
+// output: Hola mundo
