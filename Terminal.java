9c629ce8cd2879ac8d18b596e970828b630640d0
SenyuMei <SenYuMei@hotmail.com>
2025-02-18 12:04:36 +0100

    Create Terminal.java

---
+++ Terminal.java
@@ -0,0 +1,3 @@
+para compilar en la terminal se usa "javac nombre_archivo.java"
+si el compilado se logra se creara un .class "nombre_archivo.class"
+para ejecutar un .class se usa "java nombre_archivo"
