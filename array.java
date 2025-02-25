177e6c6d8f4daa57769b6184deb7a18e7a2c5c6a
SenyuMei <SenYuMei@hotmail.com>
2025-02-25 13:47:34 +0100

    Create array.java

---
+++ array.java
@@ -0,0 +1,10 @@
+int [] array = new int [n* de valores]
+// cuando se vale el valor de cada array
+int [] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+// si a una casilla de un array no es asociado a ningún valor se le es asignador 0 directamente por defecto
+int nombre_variable = nombre_matriz.length;
+
+//al añadir el .length cogeremos solo el número de variables que contenga el array 
+int num = array.length;
+System.out.println(num);
+//output = 10;
