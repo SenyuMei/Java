fbd95c0d19cfab4f41dcb0853f45e70c875518f3
SenyuMei <SenYuMei@hotmail.com>
2025-02-26 13:17:33 +0100

    Create .equals().java

---
+++ .equals().java
@@ -0,0 +1,6 @@
+// para hacer comparaciones en vez de usar == se puede usar 
+(nombre_variable1.equals(nombre_variable2);
+// es lo mismo que decir
+nombre_variable1 == nombre_variable2;
+System.out.println(nombre_variable1.equals(nombre_variable2));
+// el output sera false o true dependiendo de si la sentencia de que vairable 1 y variable sean == o !=
