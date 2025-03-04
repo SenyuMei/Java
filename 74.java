b0724a98d42f2366e5ff667c25397d21b7949697
SenyuMei <SenYuMei@hotmail.com>
2025-03-04 13:27:32 +0100

    Create 74.java

---
+++ 74.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class GeneradorAcronimo {
+    public static void main(String[] args) {
+        
+        Scanner lector = new Scanner(System.in);
+        System.out.print("Introduce un acronimo: ");
+        String text = lector.nextLine();
+        
+        String acronimo = "ETSETB";
+        String acronimoCompleto = "Escola Tècnica Superior Enginyeria Telecomunicació Barcelona";
+        String siNo = "Su acronimo no se a podido leer :-(";
+        
+        if (text.equals(acronimo) == true) {
+           System.out.println(acronimoCompleto);
+        } else {
+            System.out.println(siNo);
+        }
+    }
+}
